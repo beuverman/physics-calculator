2ea2dce54a8e77c0537629868ec1b7b284d95623
beuverman <brandoneuv@gmail.com>
2023-09-18 19:10:46 -0700

    Added big math dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module physicscalculator {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires ch.obermuhlner.math.big;
 
 
     opens ui to javafx.fxml;
