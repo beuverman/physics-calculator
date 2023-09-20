cf239e2f86a775e012cc668bee43bbde5b0824a5
beuverman <brandoneuv@gmail.com>
2023-09-19 23:33:23 -0700

    Added dynamic number of text fields

--- src/main/java/ui/EquationController.java
+++ src/main/java/ui/EquationController.java
@@ -3,21 +3,30 @@ package ui;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
 import physics.Equation;
 import physics.exceptions.IncompatibleUnitsException;
 import physics.exceptions.InvalidDimensionException;
 
 public class EquationController {
     @FXML
-    private TextField equationResult;
+    private VBox equationVBox;
+
+    @FXML
+    private VBox resultVBox;
+
+    private int size = 1;
 
     @FXML
     protected void onEquationKeyTyped(KeyEvent ke) {
         Equation eq;
         String result;
 
+        TextField box = (TextField)ke.getSource();
+        int index = equationVBox.getChildren().indexOf(box);
+
         try {
-            eq = new Equation(((TextField)ke.getSource()).getText());
+            eq = new Equation(box.getText());
             result = eq.evaluate().toString();
         }
         catch (IncompatibleUnitsException | InvalidDimensionException e) {
@@ -27,6 +36,33 @@ public class EquationController {
             result = "Error";
         }
 
-        equationResult.setText(result);
+        ((TextField)resultVBox.getChildren().get(index)).setText(result);
+
+        if (index == size - 1) {
+            if (box.getText().equals(""))
+                removeFields();
+            else
+                addFields();
+        }
+    }
+
+    private void addFields() {
+        TextField equationBox = new TextField();
+        TextField resultBox = new TextField();
+
+        resultBox.setEditable(false);
+        
+        equationBox.setOnKeyTyped(this::onEquationKeyTyped);
+        equationVBox.getChildren().add(equationBox);
+        resultVBox.getChildren().add(resultBox);
+
+        size++;
+    }
+
+    private void removeFields() {
+        equationVBox.getChildren().remove(size - 1);
+        resultVBox.getChildren().remove(size - 1);
+
+        size--;
     }
 }
