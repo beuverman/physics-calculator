b0deebe48018c1beaec4e6f4a85e80aa023edddd
beuverman <brandoneuv@gmail.com>
2024-04-27 22:37:05 -0700

    Changes to pom.xml to be able to create a runtime image

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,10 @@ module physicscalculator {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
-    requires ch.obermuhlner.math.big;
-    requires jlatexmath;
     requires java.desktop;
     requires javafx.swing;
+    requires ch.obermuhlner.math.big;
+    requires jlatexmath;
     requires jscience;
 
     opens ui to javafx.fxml;
